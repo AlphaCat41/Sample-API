fc1606d695ff3e27e6fc2918f0a2437e5f4ab3e8
Pakdee H <pakdee.hn@outlook.com>
2024-12-14 19:00:57 +0700

    [+] Add popup

--- db/users.js
+++ db/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mysql = require('mysql2/promise')
+const url = require('url');
 
 const timeLog = (req, res, next) => {
     const time = new Date()
@@ -21,7 +22,8 @@ router.get("/", async (req, res) => {
     const connection = await dbCon
     const [results, fields] = await connection.query("SELECT * FROM users")
     res.render("pages/index", {
-        "users": results
+        "users": results,
+        "message": req.query.message
     })
 })
 router.get("/:id", async (req, res) => {
@@ -34,14 +36,22 @@ router.get("/:id", async (req, res) => {
 router.post("/delete", async (req, res) => {
     const connection = await dbCon
     await connection.query("DELETE FROM users WHERE id = ?", [req.body.id])
-    // res.send("Deleted id " + req.body.id + " successful")
-    res.redirect("/users")
+    res.redirect(url.format({
+        pathname: "/users",
+        query: {
+            "message": `Deleted id (${req.body.id}) successful`
+        }
+    }));
 })
 router.post("/add", async (req, res) => {
     const connection = await dbCon
     await connection.query("INSERT INTO users (firstName, lastName, birthYear, email) VALUES (?)", [[req.body.firstName, req.body.lastName, req.body.birthYear, req.body.email]])
-    // res.send("Add user " + req.body.firstName + " successful")
-    res.redirect("/users")
+    res.redirect(url.format({
+        pathname: "/users",
+        query: {
+            "message": `Add user (${req.body.firstName}) successful`
+        }
+    }));
 })
 
 module.exports = router
---
+++ public/users/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector('.close-popup').addEventListener('click', () => {
+    console.log("test")
+    document.querySelector('.popup').style.display = 'none';
+});
